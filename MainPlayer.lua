dea1336a0ed8db05b1aae386ee43302b223bb1c1
zetoirque <89418946+zetoirque@users.noreply.github.com>
2023-03-23 18:46:20 +0100

    Update MainPlayer.lua

--- MainPlayer.lua
+++ MainPlayer.lua
@@ -1,18 +1,19 @@
+for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+    v:Disable()
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
-getgenv().FlySpeed = 10;
+getgenv().FlySpeed = 500;
+getgenv().ESPSS = true
 
 local Sound = Instance.new("Sound",game:GetService("SoundService"))
 Sound.SoundId = "rbxassetid://232127604"
 
-local Main = Library.CreateLib("SCRIPT BY ZETOIRQUE", "Synapse")
+local Main = Library.CreateLib("SCRIPT BY ZETOIRQUE", "Ocean")
 
 local MainTab = Main:NewTab("Player")
-local MainSection = MainTab:NewSection("Player Modification")
-local FlySection = MainTab:NewSection("Fly")
-local InvisibleSection = MainTab:NewSection("Invisibility")
-local TpSection = MainTab:NewSection("TP")
 
 local OtherTab = Main:NewTab("Settings")
 local OtherSection = OtherTab:NewSection("Other")
@@ -28,7 +29,32 @@ local uis = game:GetService("UserInputService")
 
 
 
+--Other Section
 
+OtherSection:NewButton("Rejoin Server", "Make You Rejoin The Server Where You Are",function()
+
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+
+end)
+
+SettingsSection:NewKeybind("ToggleUI", "", Enum.KeyCode.LeftAlt, function()
+	Library:ToggleUI()
+end)
+
+
+local themes = {
+    SchemeColor = Color3.fromRGB(46, 48, 43),
+    Background = Color3.fromRGB(13, 15, 12),
+    Header = Color3.fromRGB(36, 38, 35),
+    TextColor = Color3.fromRGB(152, 99, 53),
+    ElementColor = Color3.fromRGB(24, 24, 24)
+}
+
+for theme, color in pairs(themes) do
+    CustomisationSection:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
 
 
 --FlyScript
@@ -150,156 +176,10 @@ end)
 
 
 
-
-
-
-
---Invisibility Script
---Settings:
-local ScriptStarted = false
-local Transparency = true
- 
-local Player = game:GetService("Players").LocalPlayer
-local RealCharacter = Player.Character or Player.CharacterAdded:Wait()
- 
-local IsInvisible = false
- 
-RealCharacter.Archivable = true
-
-local FakeCharacter = RealCharacter:Clone()
-local Part
-
-Part = Instance.new("Part", workspace)
-
-Part.Anchored = true
-Part.Size = Vector3.new(200, 1, 200)
-Part.CFrame = CFrame.new(0, -500, 0)
-Part.CanCollide = true
-
-FakeCharacter.Parent = workspace
-FakeCharacter.HumanoidRootPart.CFrame = Part.CFrame * CFrame.new(0, 5, 0)
- 
-for i, v in pairs(RealCharacter:GetChildren()) do
-  if v:IsA("LocalScript") then
-      local clone = v:Clone()
-      clone.Disabled = true
-      clone.Parent = FakeCharacter
-  end
-end
-
-if Transparency then
-  for i, v in pairs(FakeCharacter:GetDescendants()) do
-      if v:IsA("BasePart") then
-          v.Transparency = 0.5
-      end
-  end
-end
-
-local CanInvis = true
-
-function RealCharacterDied()
-
-  CanInvis = false
-  RealCharacter:Destroy()
-  RealCharacter = Player.Character
-  CanInvis = true
-  isinvisible = false
-  FakeCharacter:Destroy()
-  workspace.CurrentCamera.CameraSubject = RealCharacter.Humanoid
- 
-  RealCharacter.Archivable = true
-  FakeCharacter = RealCharacter:Clone()
-  Part:Destroy()
-  Part = Instance.new("Part", workspace)
-  Part.Anchored = true
-  Part.Size = Vector3.new(200, 1, 200)
-  Part.CFrame = CFrame.new(9999, -100,-9999) --Set this to whatever you want, just far away from the map.
-  Part.CanCollide = true
-  FakeCharacter.Parent = workspace
-  FakeCharacter.HumanoidRootPart.CFrame = Part.CFrame * CFrame.new(0, 5, 0)
- 
-  for i, v in pairs(RealCharacter:GetChildren()) do
-      if v:IsA("LocalScript") then
-          local clone = v:Clone()
-          clone.Disabled = true
-          clone.Parent = FakeCharacter
-      end
-  end
-
-  if Transparency then
-      for i, v in pairs(FakeCharacter:GetDescendants()) do
-          if v:IsA("BasePart") then
-              v.Transparency = 0.7
-          end
-      end
-  end
-
- RealCharacter.Humanoid.Died:Connect(function()
-
-    RealCharacter:Destroy()
-    FakeCharacter:Destroy()
-
- end)
-
- Player.CharacterAppearanceLoaded:Connect(RealCharacterDied)
-
-end
-
-RealCharacter.Humanoid.Died:Connect(function()
-
- RealCharacter:Destroy()
- FakeCharacter:Destroy()
-
- end)
-
-Player.CharacterAppearanceLoaded:Connect(RealCharacterDied)
-
-local PseudoAnchor
-
-game:GetService("RunService").RenderStepped:Connect(
-  function()
-      if PseudoAnchor ~= nil then
-          PseudoAnchor.CFrame = Part.CFrame * CFrame.new(0, 5, 0)
-      end
-  end)
- 
-PseudoAnchor = FakeCharacter.HumanoidRootPart
-
-local function Invisible()
-
-  if IsInvisible == false then
-      local StoredCF = RealCharacter.HumanoidRootPart.CFrame
-      RealCharacter.HumanoidRootPart.CFrame = FakeCharacter.HumanoidRootPart.CFrame
-      FakeCharacter.HumanoidRootPart.CFrame = StoredCF * CFrame.new(0,2,0)
-      RealCharacter.Humanoid:UnequipTools()
-      Player.Character = FakeCharacter
-      workspace.CurrentCamera.CameraSubject = FakeCharacter.Humanoid
-      PseudoAnchor = RealCharacter.HumanoidRootPart
-      for i, v in pairs(FakeCharacter:GetChildren()) do
-          if v:IsA("LocalScript") then
-              v.Disabled = false
-          end
-      end
- 
-      IsInvisible = true
-  else
-      local StoredCF = FakeCharacter.HumanoidRootPart.CFrame
-      FakeCharacter.HumanoidRootPart.CFrame = RealCharacter.HumanoidRootPart.CFrame
- 
-      RealCharacter.HumanoidRootPart.CFrame = StoredCF * CFrame.new(0,2,0)
- 
-      FakeCharacter.Humanoid:UnequipTools()
-      Player.Character = RealCharacter
-      workspace.CurrentCamera.CameraSubject = RealCharacter.Humanoid
-      PseudoAnchor = FakeCharacter.HumanoidRootPart
-      for i, v in pairs(FakeCharacter:GetChildren()) do
-          if v:IsA("LocalScript") then
-              v.Disabled = true
-          end
-      end
-      IsInvisible = false
-  end
-end
+local MainSection = MainTab:NewSection("Player Modification")
+local ESPSection = MainTab:NewSection("ESP")
+local FlySection = MainTab:NewSection("Fly")
+local TpSection = MainTab:NewSection("TP")
 
 
 
@@ -308,6 +188,27 @@ end
 
 
 
+local esp_settings = {
+    textsize = 13,
+}
+
+local gui = Instance.new("BillboardGui")
+local esp = Instance.new("TextLabel",gui)
+
+
+
+gui.Name = "Cracked esp";
+gui.ResetOnSpawn = false
+gui.AlwaysOnTop = true;
+gui.LightInfluence = 0;
+gui.Size = UDim2.new(1.75, 0, 1.75, 0);
+esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+esp.Text = ""
+esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+esp.Font = "Arial"
+esp.TextSize = esp_settings.textsize
+esp.TextColor3 = Color3.fromRGB(255, 0, 0)
+esp.BorderSizePixel = 0
 
 
 
@@ -346,17 +247,8 @@ end)
 
 
 
-
-
-
-
-
-
---UI SCRIPTS:
-
-
-
 MainSection:NewSlider("WalkSpeed", "", 500, 15, function(s)
+    
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
@@ -375,7 +267,54 @@ MainSection:NewToggle("Click-TP", "", function(state)
 end)
 
 
-FlySection:NewKeybind("Fly", "", Enum.KeyCode.H, function()
+
+--ESP
+function ESP()
+    game:GetService("RunService").RenderStepped:Connect(function()
+
+        if ESPSS == true then
+            
+            for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+                if v ~= game:GetService("Players").LocalPlayer and v.Character and v.Character.Head and v.Character.Head:FindFirstChild("Cracked esp") == nil then
+
+
+                    esp.Text = "["..v.Name.."]".." ["..v.Character.Humanoid.MaxHealth.."]"
+                    gui:Clone().Parent = v.Character.Head
+                    
+                end
+            end
+
+        else
+
+            for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+                if v ~= game:GetService("Players").LocalPlayer and v.Character and v.Character.Head:FindFirstChild("Cracked esp")  then
+                    
+                    v.Character.Head:FindFirstChild("Cracked esp"):Destroy() 
+
+                end
+            end
+
+        end
+    end)
+end
+
+
+ESPSection:NewToggle("ESP", "", function(state)
+    if state then
+
+        ESPSS = true
+        ESP()
+
+    else
+
+        ESPSS = false
+        ESP()
+    
+    end
+end)
+
+
+FlySection:NewKeybind("Fly", "", Enum.KeyCode.E, function()
 
     if flying == true then 
 
@@ -392,22 +331,6 @@ end)
 FlySection:NewSlider("FlySpeed", "", 400, 10, function(s)
     FlySpeed = s
 end)
-
-
-InvisibleSection:NewKeybind("Invisible", "", Enum.KeyCode.G, function()
-
-    if CanInvis and RealCharacter and FakeCharacter then
-        if RealCharacter:FindFirstChild("HumanoidRootPart") and FakeCharacter:FindFirstChild("HumanoidRootPart") then
-            Invisible()
-        end
-    end
-	
-end)
-
-InvisibleSection:NewButton("Debug", "Press This Until Its Rework", function()
-    RealCharacterDied()
-end)
-
 
 
 local oldList = {"Click The Refresh Button To See The Players"}
@@ -471,46 +394,6 @@ end)
 
 
 
-
---Other Section
-
-OtherSection:NewButton("Rejoin Server", "Make You Rejoin The Server Where You Are",function()
-
-    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
-
-end)
-
-SettingsSection:NewKeybind("ToggleUI", "", Enum.KeyCode.LeftAlt, function()
-	Library:ToggleUI()
-end)
-
-
-local themes = {
-    SchemeColor = Color3.fromRGB(46, 48, 43),
-    Background = Color3.fromRGB(13, 15, 12),
-    Header = Color3.fromRGB(36, 38, 35),
-    TextColor = Color3.fromRGB(152, 99, 53),
-    ElementColor = Color3.fromRGB(24, 24, 24)
-}
-
-for theme, color in pairs(themes) do
-    CustomisationSection:NewColorPicker(theme, "Change your "..theme, color, function(color3)
-        Library:ChangeColor(theme, color3)
-    end)
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+wait(1)
 Sound:Play()
 Notification.new("success", "Script Fully Load !", "Script By Zetoirque.", true, 3)
